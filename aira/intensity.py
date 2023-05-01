222cc5521cd5586808b82e00898ecadfeda3bc13
ips <ipupkin@untref.edu.ar>
2023-05-01 11:58:01 -0300

    Adjust better naming

--- aira/intensity.py
+++ aira/intensity.py
@@ -75,48 +75,47 @@ def hamming_window(
     return Ix_vent, Iy_vent, Iz_vent
 
 
-def integrate_bformat(
-    intensity: np.ndarray,
+def integrate_bformat_intensities(
+    intensity_directions: np.ndarray,
     duration_secs: float,
     sample_rate: int,
 ) -> np.ndarray:
     """Integrate the intensity signal with Hamming windows of length `duration_secs`.
 
     Args:
-        intensity (np.ndarray): X, Y and Z intensity signals.
+        intensity_directions (np.ndarray): X, Y and Z intensity signals.
         duration_secs (float): the length of the window to apply, in seconds.
         sample_rate (int): sampling rate of the signal.
 
     Returns:
         np.ndarray: the integrated signal, of shape (3, ...)
     """
-    if intensity.shape[0] == 4:
-        intensity = intensity[1:, :]
-    elif (intensity.shape[0] < 3) or (intensity.shape[0] > 4):
-        raise ValueError(f"Unexpected input shape {intensity.shape}")
+    if intensity_directions.shape[0] == 4:
+        intensity_directions = intensity_directions[1:, :]
+    elif (intensity_directions.shape[0] < 3) or (intensity_directions.shape[0] > 4):
+        raise ValueError(f"Unexpected input shape {intensity_directions.shape}")
 
     # Convert integration time to samples
     duration_samples = np.round(duration_secs * sample_rate).astype(np.int64)
     duration_samples = duration_samples.astype(np.int64)
 
-    # Peak scanning: keep only from the highest peak to the end of the signal
-    earliest_peak_index = np.argmax(intensity, axis=1).min()
-    intensity = intensity[:, earliest_peak_index:]
+    # Peak scanning: keep only from the leftmost highest peak to the end of the signal
+    earliest_peak_index = np.argmax(intensity_directions, axis=1).min()
+    intensity_directions = intensity_directions[:, earliest_peak_index:]
 
     # Windowing
-    intensity = np.concatenate(
-        [intensity, np.zeros((3, intensity.shape[1] % duration_samples))],
+    intensity_directions = np.concatenate(
+        [intensity_directions, np.zeros((3, intensity_directions.shape[1] % duration_samples))],
         axis=1,
     )  # Padding
-    output_shape = (3, intensity.shape[1] // duration_samples)
+    output_shape = (3, intensity_directions.shape[1] // duration_samples)
     intensity_windowed = np.zeros(output_shape)
     window = np.hamming(duration_samples)
     for i in range(0, output_shape[1]):
-        intensity_segment = intensity[:, i : i + duration_samples]
+        intensity_segment = intensity_directions[:, i : i + duration_samples]
         intensity_windowed[:, i] = np.sum(
-            intensity_segment * window,
-            axis=1
-        )  # TODO: verify that simple addition is ok. Might need to be energy...
+            intensity_segment * window, axis=1
+        )  # TODO: reading Cacavelos' thesis, the division by the number of samples is missing here
 
     return intensity_windowed
 
@@ -195,20 +194,18 @@ def convert_bformat_to_intensity(
     signal_filtered = apply_low_pass_filter(signal, cutoff_frequency, sample_rate)
 
     # Calculate intensity from directions
-    signal_filtered[1:, :] = (
+    intensity_directions = (
         signal_filtered[0, :] * signal_filtered[1:, :]
-    )  # W channel * Other channels
-    # TODO: shouldn't it be a correlation
-
-    intensity_windowed = integrate_bformat(
-        signal_filtered[1:, :],  # drop W
+    )  # Intensity = pressure (W channel) * pressure gradient (XYZ channels)
+    intensity_windowed = integrate_bformat_intensities(
+        intensity_directions,
         duration_secs=integration_time,
         sample_rate=sample_rate,
     )
 
     # Convert to total intensity, azimuth and elevation
     intensity = np.sqrt((intensity_windowed**2).sum(axis=0)).squeeze()
-    azimuth = np.rad2deg(np.arctan(intensity_windowed[1] / intensity_windowed[0]))
-    elevation = np.rad2deg(np.arctan(intensity_windowed[2] / intensity))
+    azimuth = np.rad2deg(np.arctan(intensity_windowed[1] / intensity_windowed[0])).squeeze()
+    elevation = np.rad2deg(np.arctan(intensity_windowed[2] / intensity)).squeeze()
 
     return intensity, azimuth, elevation
