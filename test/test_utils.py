f3b56dd632ef44fcfe5ad98ca82e83a40214129f
Nahuel Passano <passano43632@estudiantes.untref.edu.ar>
2023-05-01 22:29:02 -0300

    add: test_utils.py

---
+++ test/test_utils.py
@@ -0,0 +1,22 @@
+from aira.utils import read_audio
+import soundfile as sf
+
+
+def test_read_audio():
+    audio_path_1 = "test/data/soundfield_measurements/soundfield_bld.wav"
+    audio_path_2 = "test/data/soundfield_measurements/soundfield_bru.wav"
+    audio_path_list = [audio_path_1, audio_path_2]
+
+    # Expected values
+    expected_audio_1, _ = sf.read(audio_path_1)
+    expected_audio_2, expected_sample_rate = sf.read(audio_path_2)
+    expected_audio_array_shape = (len(audio_path_list), len(expected_audio_2))
+
+    sample_rate, audio_array = read_audio(audio_path_list)
+
+    assert (
+        expected_audio_array_shape == audio_array.shape
+    ), f"Output shape: {audio_array.shape} != Expected shape: {expected_audio_array_shape}"
+    assert (
+        expected_sample_rate == sample_rate
+    ), f"Output sample rate: {sample_rate} != Expected sample rate: {expected_sample_rate}"
