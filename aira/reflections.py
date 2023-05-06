3388ef0b158aad7809ae2f31a32cd9976482a0bf
ips <ipupkin@untref.edu.ar>
2023-05-06 19:10:04 -0300

    add initial version of hedgehog processing

--- aira/reflections.py
+++ aira/reflections.py
@@ -1,7 +1,11 @@
 from enum import Enum
 from abc import ABC, abstractstaticmethod
+from typing import Tuple, Union
+
 import numpy as np
 from scipy.signal import find_peaks
+
+from aira.intensity import convert_bformat_to_intensity
 
 
 class ReflectionDetectionStrategy(ABC):
@@ -33,10 +37,41 @@ class NeighborReflectionDetectionStrategy(ReflectionDetectionStrategy):
         Returns:
             np.ndarray: an array with the indeces of the peaks.
         """
-        return find_peaks(intensity_magnitude)[0][1:]  # Drop peak properties ([0]) and direct sound peak ([1])
+        return find_peaks(intensity_magnitude)[0][
+            1:
+        ]  # Drop peak properties ([0]) and direct sound peak ([1])
 
 
 class ReflectionDetectionStrategies(Enum):
     CORRELATION = CorrelationReflectionDetectionStrategy
     THRESHOLD = ThresholdReflectionDetectionStrategy
     SCIPY = NeighborReflectionDetectionStrategy
+
+
+def get_hedgehog_arrays(
+    intensity: np.ndarray,
+    azimuth: np.ndarray,
+    elevation: np.ndarray,
+    detection_strategy: Union[
+        ReflectionDetectionStrategy, ReflectionDetectionStrategies
+    ] = NeighborReflectionDetectionStrategy,
+) -> Tuple[np.ndarray]:
+    """Analyze the normalized intensity, azimuth and elevation arrays to look for
+    reflections. Timeframes which don't contain a reflection are masked.
+
+    Args:
+        intensity (np.ndarray): normalized intensity array.
+        azimuth (np.ndarray): array with horizontal angles with respect to the XZ plane.
+        elevation (np.ndarray): array with vertical angles with respect to the XY plane.
+
+    Returns:
+        intensity (np.ndarray): masked intensities with only reflections different than 0.
+        azimuth (np.ndarray): masked intensities with only reflections different than 0.
+        elevation (np.ndarray): masked intensities with only reflections different than 0.
+    """
+    if isinstance(detection_strategy, ReflectionDetectionStrategies):
+        detection_strategy = detection_strategy.value
+    reflections_indeces = detection_strategy.get_indeces_of_reflections(intensity)
+    mask = np.zeros_like(intensity)
+    mask[reflections_indeces] = 1
+    return mask * intensity, mask * azimuth, mask * elevation
