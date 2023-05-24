3b571a5a7e91eb92d0f46b2599fc7c4ac841818d
nahue-passano <passano43632@estudiantes.untref.edu.ar>
2023-05-24 20:43:26 -0300

    update: input strategies

--- aira/engine/input.py
+++ aira/engine/input.py
@@ -7,6 +7,7 @@ import numpy as np
 from aira.engine.filtering import NonCoincidentMicsCorrection, convolve
 from aira.utils import convert_ambisonics_a_to_b
 
+
 # pylint: disable=too-few-public-methods
 class InputStrategy(ABC):
     """Base interface for a reflection detection algorithm."""
@@ -14,20 +15,27 @@ class InputStrategy(ABC):
     @abstractmethod
     @staticmethod
     def get_ir_array(signals_array: np.ndarray, sample_rate: int) -> np.ndarray:
-        """Abstract method to be overwritten by concrete implementations of 
+        """Abstract method to be overwritten by concrete implementations of
         input"""
 
 
 # pylint: disable=too-few-public-methods
 class AFormatInputStrategy(InputStrategy):
-    """Algorithm to get an array containing the signals in B-Format from 
+    """Algorithm to get an array containing the signals in B-Format from
     A-Format"""
 
     @staticmethod
     def get_ir_array(signals_array: np.ndarray, sample_rate: int) -> np.ndarray:
         bformat_signals = convert_ambisonics_a_to_b(signals_array)
         frequency_corrector = NonCoincidentMicsCorrection(sample_rate)
-        frequency_corrector.correct_omni()
+        bformat_corrected = np.zeros_like(bformat_signals)
+        bformat_corrected[0, :] = frequency_corrector.correct_omni(
+            bformat_signals[0, :]
+        )
+        bformat_corrected[1:, :] = frequency_corrector.correct_axis(
+            bformat_signals[1:, :]
+        )
+        return bformat_corrected
 
 
 # pylint: disable=too-few-public-methods
@@ -41,12 +49,12 @@ class BFormatInputStrategy(InputStrategy):
 
 # pylint: disable=too-few-public-methods
 class LSSInputStrategy(InputStrategy):
-    """Algorithm to get an array containing the signals in B-Format from 
+    """Algorithm to get an array containing the signals in B-Format from
     LSS measurements with their inverse filter"""
 
     @staticmethod
     def get_ir_array(signals_array: np.ndarray) -> np.ndarray:
-        pass
+        raise NotImplementedError("Available inputs are in A-Format and B-Format")
 
 
 class InputStrategies(Enum):
