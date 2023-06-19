bb9efe4a237135fd8888e7003a903e04b990087d
nahue-passano <passano43632@estudiantes.untref.edu.ar>
2023-06-19 02:10:11 -0300

    update: modularized integrate_intensity_directions and added analysis crop and threholding functions

--- aira/engine/intensity.py
+++ aira/engine/intensity.py
@@ -9,6 +9,63 @@ from aira.engine.filtering import apply_low_pass_filter
 FILTER_CUTOFF = 5000
 
 
+def analysis_crop(
+    analysis_length: float,
+    sample_rate: int,
+    intensity_directions: np.ndarray,
+):
+    """_summary_
+
+    Parameters
+    ----------
+    analysis_length : float
+        _description_
+    sample_rate : int
+        _description_
+    intensity_directions : np.ndarray
+        _description_
+
+    Returns
+    -------
+    _type_
+        _description_
+    """
+    # Get analysis length max index
+    analysis_length_idx = int(analysis_length * sample_rate)
+
+    # Slice from intensity max to analysis length from intensity max
+    earliest_peak_index = np.argmax(np.abs(intensity_directions), axis=1).min()
+    intensity_directions_cropped = intensity_directions[
+        :, earliest_peak_index : earliest_peak_index + analysis_length_idx
+    ]
+
+    return intensity_directions_cropped
+
+
+def intensity_thresholding(
+    threshold: float,
+    intensity: np.ndarray,
+    azimuth: np.ndarray,
+    elevation: np.ndarray,
+    reflections: np.ndarray,
+) -> Tuple[np.ndarray]:
+    """_summary_
+
+    Parameters
+    ----------
+    threshold : _type_
+        _description_
+    """
+    reflex_to_direct = intensity_to_dB(intensity) - intensity_to_dB(intensity[0])
+    thresholding_mask = reflex_to_direct > threshold
+    return (
+        reflex_to_direct[thresholding_mask],
+        azimuth[thresholding_mask],
+        elevation[thresholding_mask],
+        reflections[thresholding_mask],
+    )
+
+
 def intensity_to_dB(intensity_array: np.ndarray) -> np.ndarray:
     """Converts intensity to dB scale using 1e-12 as intensity reference
 
@@ -22,7 +79,7 @@ def intensity_to_dB(intensity_array: np.ndarray) -> np.ndarray:
     np.ndarray
         Intensity array in dB scale
     """
-    return 10 * np.log10(intensity_array / 1e-12)
+    return 20 * np.log10(intensity_array / 1e-12)
 
 
 def min_max_normalization(array: np.ndarray) -> np.ndarray:
@@ -64,10 +121,6 @@ def integrate_intensity_directions(
     # Convert integration time to samples
     duration_samples = np.round(duration_secs * sample_rate).astype(np.int64)
 
-    # Peak scanning: keep only from the leftmost highest peak to the end of the signal
-    earliest_peak_index = np.argmax(np.abs(intensity_directions), axis=1).min()
-    intensity_directions = intensity_directions[:, earliest_peak_index:]
-
     # Padding and windowing
     intensity_directions = np.concatenate(
         [
@@ -76,17 +129,14 @@ def integrate_intensity_directions(
         ],
         axis=1,
     )
-    output_shape = (3, intensity_directions.shape[1] // duration_samples)
+    output_shape = (3, intensity_directions.shape[1] - duration_samples + 1)
     intensity_windowed = np.zeros(output_shape)
     window = np.hamming(duration_samples)
     for i in range(0, output_shape[1]):
-        # intensity_segment = intensity_directions[
-        #    :, i * duration_samples : (i + 1) * duration_samples
-        # ]
-        intensity_segment = intensity_directions[
-            :, i : i + duration_samples
-        ]
-        intensity_windowed[:, i] = np.sum(intensity_segment * window, axis=1)
+        intensity_segment = intensity_directions[:, i : i + duration_samples]
+        intensity_windowed[:, i] = (
+            np.sum(intensity_segment * window, axis=1) / duration_samples
+        )
 
     return intensity_windowed
 
@@ -94,7 +144,6 @@ def integrate_intensity_directions(
 def convert_bformat_to_intensity(
     signal: np.ndarray,
     sample_rate: int,
-    integration_time: int,
     cutoff_frequency: int = FILTER_CUTOFF,
 ) -> Tuple[np.ndarray]:
     """Integrate and compute intensities for a B-format Ambisonics recording.
@@ -102,30 +151,39 @@ def convert_bformat_to_intensity(
     Args:
         signal (np.ndarray): input B-format Ambisonics signal. Shape: (4, N).
         sample_rate (int): sampling rate of the signal.
-        integration_time (int): integration time to apply, in seconds.
         cutoff_frequency (int, optional): cutoff frequency for the low-pass
         filter. Defaults to 5000 Hz.
 
     Returns:
         Tuple[np.ndarray]: integrated intensity, azimuth and elevation.
     """
-    signal_filtered = apply_low_pass_filter(signal, cutoff_frequency, sample_rate)
+    # signal_filtered = apply_low_pass_filter(signal, cutoff_frequency, sample_rate)
+    signal_filtered = signal
 
     # Calculate intensity from directions
     intensity_directions = (
         signal_filtered[0, :] * signal_filtered[1:, :]
     )  # Intensity = pressure (W channel) * pressure gradient (XYZ channels)
-    intensity_windowed = integrate_intensity_directions(
-        intensity_directions,
-        duration_secs=integration_time,
-        sample_rate=sample_rate,
-    )
+    return intensity_directions
 
+
+def get_intensity_polar_data(intensity_windowed: np.ndarray):
+    """_summary_
+
+    Parameters
+    ----------
+    intensity_windowed : np.ndarray
+        _description_
+
+    Returns
+    -------
+    _type_
+        _description_
+    """
     # Convert to total intensity, azimuth and elevation
     intensity = np.sqrt((intensity_windowed**2).sum(axis=0)).squeeze()
     azimuth = np.rad2deg(
         np.arctan(intensity_windowed[1] / intensity_windowed[0])
     ).squeeze()
     elevation = np.rad2deg(np.arccos(intensity_windowed[2] / intensity)).squeeze()
-
     return intensity, azimuth, elevation
